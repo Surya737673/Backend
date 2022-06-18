fdcaa591fed1ba0d1905bfb213212d37210772ba
Surya737673 <99668540+Surya737673@users.noreply.github.com>
2022-06-18 23:59:58 +0530

    movies

--- Sprint4Day2/index.js
+++ Sprint4Day2/index.js
@@ -1,22 +1,54 @@
-const {Movie, connection} = require('./index')
-const express= require("express");
-
-const app=express();
+const {Movie,connection}=require("./db")
+const express=require("express")
+const app=express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-app.post("/movie",async (req,res)=>{
-  const MoviesData=await Movie.insertMany([req.body]);
-  res.send()
-               
-})
-app.get("/movie", async (req,res)=>{
-  const MoviesData=await Movie.find();
-  res.send()
-               
+
+app.get("/",async(req,res)=>{
+    const params=req.query
+    if(params){
+        const movies=await Movie.find(params)
+        return res.json(movies)
+    }
+    const movies=await Movie.find()
+    return res.json(movies)
 })
 
-app.listen(8080, async ()=>{
-    await connection;
-    console.log(" server started ")
+
+// post Data
+app.post("/movies",async(req,res)=>{
+    await Movie.insertMany([req.body])
+    res.send("Data has been Posted")
+})
+
+
+
+
+app.patch("/movies/:id",async(req,res)=>{
+    const {id}=req.params
+    let updateObject =req.body
+    let movies=await Movie.updateMany({_id :id},{$set: updateObject});
+    return res.json(movies)
+})
+
+// delete data
+app.delete("/movies/:id",async(req,res)=>{
+    const {id}=req.params
+    await Movie.deleteMany({_id:id})
+    res.send(`document deleted successfully-->id-->${id}`)
+})
+
+app.post("/movies",async (req,res)=>{
+  const q=req.query;
+  const data=await Movie.find({"title":{"$rogex":q,'options':'i'}},{})
+})
+
+app.listen(8080, async()=>{
+    try{
+        await connection
+        console.log("Connected to db");
+    }catch{
+        console.log("failed to connect to db");
+    }
 })
